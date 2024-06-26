3d5a1f9f5ced4281fae92f87412dc2d83c62a66e
JAB <jbestudiosycapacitaciones@gmail.com>
2024-06-26 16:29:22 -0300

    se agregaron botones a formulario inicio-cierre

--- general/clases/footer.cs
+++ general/clases/footer.cs
@@ -11,19 +11,25 @@ using System.Windows.Forms;
 
 namespace Ofelia_Sara.general.clases
 {
-    // NO SE ESTAN APLICANDO MODIFICACIONES DE POSICION DERECHA, TAMAÑO, SI SE MODIFICA PONERLO MAS ARRIBA
     public static class FooterHelper
     {
         public static Label CreateFooterLabel(Form form)
         {
             Label footerLabel = new Label();
             footerLabel.AutoSize = true;
-            footerLabel.Location = new System.Drawing.Point(10, form.ClientSize.Height -5 );
+            footerLabel.Location = new System.Drawing.Point(50, form.ClientSize.Height - 50); // Ajusta la ubicación en Y
             footerLabel.Name = "footerLabel";
             footerLabel.Size = new System.Drawing.Size(200, 16);
             footerLabel.TabIndex = 0;
             footerLabel.Text = "Desarrollado por Jorge A. Bonato - V. 1.0.0";
-            footerLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            footerLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right; // Anclar a la parte inferior y derecha
+
+            // Ajustar la posición para que siempre esté cerca del borde inferior
+            form.Controls.Add(footerLabel);
+            form.Resize += (sender, e) =>
+            {
+                footerLabel.Location = new System.Drawing.Point(form.ClientSize.Width - footerLabel.Width - 10, form.ClientSize.Height - footerLabel.Height - 10);
+            };
 
             return footerLabel;
         }
--- inicio_cierre.Designer.cs
+++ inicio_cierre.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InicioCierre));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -56,6 +57,10 @@
             this.comboBox10 = new System.Windows.Forms.ComboBox();
             this.label13 = new System.Windows.Forms.Label();
             this.comboBox11 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -292,11 +297,61 @@
             this.comboBox11.Size = new System.Drawing.Size(45, 21);
             this.comboBox11.TabIndex = 4;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(59, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 67);
+            this.button1.TabIndex = 27;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(159, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 67);
+            this.button2.TabIndex = 28;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(258, 364);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 67);
+            this.button3.TabIndex = 29;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(368, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 83);
+            this.button4.TabIndex = 30;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
             // InicioCierre
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(502, 504);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox11);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.comboBox10);
@@ -328,6 +383,38 @@
             this.Name = "InicioCierre";
             this.Text = "INICIO - CIERRE";
             this.Load += new System.EventHandler(this.InicioCierreLoad);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.label9, 0);
+            this.Controls.SetChildIndex(this.label10, 0);
+            this.Controls.SetChildIndex(this.textBox2, 0);
+            this.Controls.SetChildIndex(this.textBox3, 0);
+            this.Controls.SetChildIndex(this.textBox4, 0);
+            this.Controls.SetChildIndex(this.comboBox2, 0);
+            this.Controls.SetChildIndex(this.comboBox3, 0);
+            this.Controls.SetChildIndex(this.comboBox4, 0);
+            this.Controls.SetChildIndex(this.comboBox5, 0);
+            this.Controls.SetChildIndex(this.label11, 0);
+            this.Controls.SetChildIndex(this.comboBox6, 0);
+            this.Controls.SetChildIndex(this.label12, 0);
+            this.Controls.SetChildIndex(this.comboBox7, 0);
+            this.Controls.SetChildIndex(this.comboBox8, 0);
+            this.Controls.SetChildIndex(this.comboBox9, 0);
+            this.Controls.SetChildIndex(this.comboBox10, 0);
+            this.Controls.SetChildIndex(this.label13, 0);
+            this.Controls.SetChildIndex(this.comboBox11, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.Controls.SetChildIndex(this.button3, 0);
+            this.Controls.SetChildIndex(this.button4, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -363,5 +450,9 @@
         private System.Windows.Forms.ComboBox comboBox10;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.ComboBox comboBox11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
