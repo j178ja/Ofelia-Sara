76ee88caef3b00440857694df509eaf3aea4bfe8
JAB <jbestudiosycapacitaciones@gmail.com>
2024-08-06 17:19:54 -0300

    seagrego comportamiento estetico picktureBox en AGREGAR NUEVO INSTRUCTOR

--- general/clases/Agregar Componentes/NuevoSecretario.cs
+++ general/clases/Agregar Componentes/NuevoSecretario.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using SkiaSharp;//biblioteca para efecto visual
+//using SkiaSharp;//biblioteca para efecto visual
 using System.Drawing.Imaging;
 
 
