db7bf909e4ed68770bbfe1a41cc577a495c3114e
JAB <jbestudiosycapacitaciones@gmail.com>
2024-08-08 01:15:36 -0300

    se crearon mensajes de confirmacion para calendarioForm

--- general/clases/Apariencia General/Controles/CalendarForm.Designer.cs
+++ general/clases/Apariencia General/Controles/CalendarForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ofelia_Sara.general.clases.Apariencia_General.Controles
 {
-    partial class CalendarForm
+    partial class CALENDARIO
     {
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
@@ -18,48 +18,63 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CALENDARIO));
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.buttonOk = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            // 
             // monthCalendar1
+            // 
             this.monthCalendar1.Location = new System.Drawing.Point(12, 12);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 0;
-
+            // 
             // buttonOk
+            // 
             this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonOk.Location = new System.Drawing.Point(12, 182);
+            this.buttonOk.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonOk.Location = new System.Drawing.Point(187, 182);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(75, 23);
             this.buttonOk.TabIndex = 1;
             this.buttonOk.Text = "OK";
             this.buttonOk.UseVisualStyleBackColor = true;
             this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
-
+            // 
             // buttonCancel
+            // 
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(93, 182);
+            this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonCancel.Location = new System.Drawing.Point(12, 182);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 2;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
             this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
-
-            // CalendarForm
+            // 
+            // CALENDARIO
+            // 
             this.AcceptButton = this.buttonOk;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(154)))), ((int)(((byte)(174)))));
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(180, 210);
+            this.ClientSize = new System.Drawing.Size(274, 210);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOk);
             this.Controls.Add(this.monthCalendar1);
-            this.Name = "CalendarForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Select Date";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CALENDARIO";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALENDARIO";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.CALENDARIO_HelpButtonClicked);
             this.Load += new System.EventHandler(this.CalendarForm_Load);
             this.ResumeLayout(false);
+
         }
     }
 }
--- general/clases/Apariencia General/Controles/CalendarForm.cs
+++ general/clases/Apariencia General/Controles/CalendarForm.cs
@@ -10,11 +10,11 @@ using System.Windows.Forms;
 
 namespace Ofelia_Sara.general.clases.Apariencia_General.Controles
 {
-    public partial class CalendarForm : Form
+    public partial class CALENDARIO : Form
     {
         public DateTime SelectedDate { get; private set; }
 
-        public CalendarForm()
+        public CALENDARIO()
         {
             InitializeComponent();
         }
@@ -27,6 +27,7 @@ namespace Ofelia_Sara.general.clases.Apariencia_General.Controles
         private void buttonOk_Click(object sender, EventArgs e)
         {
             SelectedDate = monthCalendar1.SelectionStart; // Obtiene la fecha seleccionada
+            MessageBox.Show("Seleccion guardada.", "Confirmación   Ofelia-Sara", MessageBoxButtons.OK, MessageBoxIcon.Information);
             this.DialogResult = DialogResult.OK;
             this.Close();
         }
@@ -35,6 +36,15 @@ namespace Ofelia_Sara.general.clases.Apariencia_General.Controles
         {
             this.DialogResult = DialogResult.Cancel;
             this.Close();
+        }
+
+        private void CALENDARIO_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            // Mostrar un mensaje de ayuda
+            MessageBox.Show("Haga click sobre año, luego mes, y finalice con el dia" , "Ayuda", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Cancelar el evento para que no se cierre el formulario
+            e.Cancel = true;
         }
     }
 }
