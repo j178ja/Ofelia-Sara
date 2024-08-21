f4bbd98419357c37b9b9ec87dde31e9169a62f10
JAB <jbestudiosycapacitaciones@gmail.com>
2024-08-21 16:49:58 -0300

    se realizo cambio de posicion de btn y validacion ingreso de datos legajo, en form NuevoInstructor y NuevoSecretario

--- general/clases/Agregar Componentes/NuevoInstructor.Designer.cs
+++ general/clases/Agregar Componentes/NuevoInstructor.Designer.cs
@@ -245,7 +245,7 @@
             this.btn_Limpiar.BackColor = System.Drawing.Color.SkyBlue;
             this.btn_Limpiar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Limpiar.Image = ((System.Drawing.Image)(resources.GetObject("btn_Limpiar.Image")));
-            this.btn_Limpiar.Location = new System.Drawing.Point(156, 359);
+            this.btn_Limpiar.Location = new System.Drawing.Point(79, 359);
             this.btn_Limpiar.Name = "btn_Limpiar";
             this.btn_Limpiar.Size = new System.Drawing.Size(75, 67);
             this.btn_Limpiar.TabIndex = 6;
