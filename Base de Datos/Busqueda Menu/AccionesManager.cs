9bc2bba80406d49fac8e432148683bff41e1a5bd
JAB <jbestudiosycapacitaciones@gmail.com>
2024-08-28 12:48:53 -0300

    se agrego lista desplegable y comprtamiento a btn_Buscar en formulario menuPrincipal

---
+++ Base de Datos/Busqueda Menu/AccionesManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class AccionesManager
+{
+    public List<string> Acciones { get; private set; }
+
+    public AccionesManager(string filePath)
+    {
+        // Leer el archivo JSON y deserializarlo
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            var accionesData = JsonSerializer.Deserialize<AccionesData>(json);
+            Acciones = accionesData?.acciones ?? new List<string>();
+        }
+        else
+        {
+            Acciones = new List<string>();
+        }
+    }
+
+    private class AccionesData
+    {
+        public List<string> acciones { get; set; }
+    }
+}
