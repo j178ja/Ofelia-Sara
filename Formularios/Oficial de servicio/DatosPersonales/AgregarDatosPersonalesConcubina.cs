a3c27b642a019c6992535b6095a16cd06802b7a5
JAB <jbestudiosycapacitaciones@gmail.com>
2024-08-29 17:08:54 -0300

    solucion  a problema de textBox Domicilio en form agregar datos imputado y victima

--- Formularios/Oficial de servicio/DatosPersonales/AgregarDatosPersonalesConcubina.cs
+++ Formularios/Oficial de servicio/DatosPersonales/AgregarDatosPersonalesConcubina.cs
@@ -16,6 +16,9 @@ namespace Ofelia_Sara.Formularios.Oficial_de_servicio.DatosPersonales
         public AgregarDatosPersonalesConcubina()
         {
             InitializeComponent();
+
+            Color customBorderColor = Color.FromArgb(0, 154, 174);
+            panel1.ApplyRoundedCorners(borderRadius: 15, borderSize: 7, borderColor: customBorderColor);
         }
 
         private void AgregarDatosPersonalesConcubina_Load(object sender, EventArgs e)
