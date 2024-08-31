5449070afa90fe3743f73fd7eb48a05c5a76c9a8
JAB <jbestudiosycapacitaciones@gmail.com>
2024-08-30 21:58:22 -0300

    cambios tras skaynet

--- Formularios/Oficial de servicio/menu_principal.cs
+++ Formularios/Oficial de servicio/menu_principal.cs
@@ -113,14 +113,7 @@ namespace Ofelia_Sara
             this.WindowState = FormWindowState.Minimized;//cierra formulario actual
         }
 
-        //------BOTON IPP-----------------
-        private void Btn_Ipp_Click(object sender, EventArgs e)
-        {
-            // Crea una instancia del formulario a abrir
-            Ipp IppForm = new Ipp();
-            // Mostrar el formulario
-            IppForm.Show();
-        }
+     
 
         //-------------BOTON CONTRAVENCIONES------------
         private void Btn_Contravenciones_Click(object sender, EventArgs e)
@@ -146,52 +139,8 @@ namespace Ofelia_Sara
             this.WindowState = FormWindowState.Minimized;//cierra formulario actual
         }
 
-        //-----------BOTON LEGAJO DETENIDOS-----------
-        private void Btn_LegajoDetenidos_Click(object sender, EventArgs e)
-        {
-            // Crea una instancia del formulario a abrir
-            LegajoDetenidos LegajoDetenidosForm = new LegajoDetenidos();
-            // Mostrar el formulario
-            LegajoDetenidosForm.Show();
-        }
-
-        //-----------BOTON LEGAJO AUTOMOTOR-----------
-        private void Btn_LegajoAutomotor_Click(object sender, EventArgs e)
-        {
-            // Crea una instancia del formulario a abrir
-            LegajoAutomotor LegajoAutomotorForm = new LegajoAutomotor();
-            // Mostrar el formulario
-            LegajoAutomotorForm.Show();
-        }
-
-        //---------BOTON DENUNCIAS-------------
-        private void Btn_Denuncias_Click(object sender, EventArgs e)
-        {
-            // Crea una instancia del formulario a abrir
-            Denuncias DenunciasForm = new Denuncias();
-            // Mostrar el formulario
-            DenunciasForm.Show();
-        }
-
-        //---------BOTON NOTAS------------------
-        private void Btn_Notas_Click(object sender, EventArgs e)
-        {
-            // Crea una instancia del formulario a abrir
-            Notas NotasForm = new Notas();
-            // Mostrar el formulario
-            NotasForm.Show();
-        }
-
-        //----------BOTON INSPECCIONES-------------
-        private void Btn_Inspecciones_Click(object sender, EventArgs e)
-        {
-            // Crea una instancia del formulario a abrir
-            Inspecciones InspeccionesForm = new Inspecciones();
-            // Mostrar el formulario
-            InspeccionesForm.Show();
-        }
-
-       
+    
+     
 
         //------------- BOTON BUSCAR--------------------------
         private BindingSource bindingSource;
